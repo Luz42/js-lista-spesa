9d21eb706f0ca74adcf3c239d565d9bbf28186e3
Luz42 <a.evangelista93@gmail.com>
2022-08-30 18:25:50 +0200

    STEP 2 stampati i risultati sull'html ed aggiunto stile

--- myScrypt.js
+++ myScrypt.js
@@ -15,24 +15,36 @@ Consigli:
 
 
 //creo la lista della spesa
-const list = ['pane', 'acqua', 'riso', 'limone']
+const list = ['pane', 'acqua', 'riso', 'limone', 'pasta', 'sapone']
 console.log(list)
+
+let containerHTML = document.querySelector('.ae-lista')
+console.log(containerHTML)
 
 //inizializzo il contatore (NECESSARIO) che verrà incrementato ad ogni ciclo
 let i = 0
 console.log(i)
-
 //il contatore andrà a selezionare un elemento diverso della lista ad ogni ciclo, il quale verrà stampato
 while(i < list.length){
    //fino al verificarsi della condizione
-
-    console.log(list[i])
+    containerHTML.innerHTML += `<li class="border-bottom border-dark py-2 border-2">${list[i]}</li>`
     //questo comando viene eseguito ad ogni ciclo
 
     i++
     //successivamente viene eseguito questo comando, NECESSARIO per far funzionare e poter uscire dal ciclo
 }
 
-//creare un elemento html
 
-//inserirci all'interno i nomi della lista
+// const clickAddElement = document.getElementById('requestButton')
+// //al click
+// clickAddElement.addEventListener('click',
+//     function(){
+
+//         const newElement = document.getElementById('newElement')
+//         let addElement = newElement.value
+//         const newList = list.push(addElement)
+//         console.log(newList)
+// })
+//prende il valore dell'input
+
+//lo inserisce nella lista
